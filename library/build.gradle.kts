302e6fc490da3d282f0c6a0b351d266aad22d866
jeffdcamp <jeffdcamp@gmail.com>
2022-10-26 22:00:15 -0600

    Updated versions

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     `maven-publish`
     signing
     kotlin("android")
-    id("de.undercouch.download") version "5.2.1"
+    id("de.undercouch.download") version "5.3.0"
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.detekt)
 }
