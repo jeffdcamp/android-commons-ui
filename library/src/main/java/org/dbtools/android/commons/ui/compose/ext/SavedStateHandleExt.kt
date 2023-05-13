1ec0b7b5b89a0fc8149c5e4954a169e2f8a0fda8
jeffdcamp <jeffdcamp@gmail.com>
2023-05-13 11:54:52 -0600

    1.7.4 / Added SavedStateHandleExt / Fixes for androidxNavigation 2.6.0-beta01

--- library/src/main/java/org/dbtools/android/commons/ui/compose/ext/SavedStateHandleExt.kt
+++ library/src/main/java/org/dbtools/android/commons/ui/compose/ext/SavedStateHandleExt.kt
@@ -7,16 +7,10 @@ import java.net.URLDecoder
 @VisibleForTesting
 fun createSaveStateErrorMessage(key: String) = "Missing SavedState value for Key: $key"
 
-fun SavedStateHandle.requireInt(key: String): Int = requireNotNull(get<Int>(key)) { createSaveStateErrorMessage(key) }
-fun SavedStateHandle.requireString(key: String): String = requireNotNull(get<String>(key)) { createSaveStateErrorMessage(key) }
+fun <T> SavedStateHandle.require(key: String): T = requireNotNull(get<T>(key)) { createSaveStateErrorMessage(key) }
 
-fun SavedStateHandle.decodedString(key: String): String? = get<String>(key)?.let { URLDecoder.decode(it, "utf-8") }
+inline fun <KEY, T> SavedStateHandle.getValueClass(key: String, mapToValueClass: (KEY) -> T?): T? = get<KEY>(key)?.let { mapToValueClass(it) }
+inline fun <KEY, T> SavedStateHandle.requireValueClass(key: String, mapToValueClass: (KEY) -> T?): T = requireNotNull(getValueClass(key, mapToValueClass)) { createSaveStateErrorMessage(key) }
+
+fun SavedStateHandle.getDecodedString(key: String): String? = get<String>(key)?.let { URLDecoder.decode(it, "utf-8") }
 fun SavedStateHandle.requireDecodedString(key: String): String = URLDecoder.decode(requireNotNull(get<String>(key)) { createSaveStateErrorMessage(key) }, "utf-8")
-
-inline fun <T> SavedStateHandle.getValueClassString(key: String, mapToValueClass: (String) -> T?): T? = getValueClass<T, String>(key, mapToValueClass)
-inline fun <T> SavedStateHandle.requireValueClassString(key: String, mapToValueClass: (String) -> T?): T = requireValueClass<T, String>(key, mapToValueClass)
-inline fun <T> SavedStateHandle.getValueClassInt(key: String, mapToValueClass: (Int) -> T?): T? = getValueClass<T, Int>(key, mapToValueClass)
-inline fun <T> SavedStateHandle.requireValueClassInt(key: String, mapToValueClass: (Int) -> T?): T = requireValueClass<T, Int>(key, mapToValueClass)
-
-inline fun <T, S> SavedStateHandle.getValueClass(key: String, mapToValueClass: (S) -> T?): T? = get<S>(key)?.let { mapToValueClass(it) }
-inline fun <T, S> SavedStateHandle.requireValueClass(key: String, mapToValueClass: (S) -> T?): T = requireNotNull(getValueClass(key, mapToValueClass)) { createSaveStateErrorMessage(key) }
