099baa37555c3a2ed62fbfdee5e1c32f569fee46
jeffdcamp <jeffdcamp@gmail.com>
2023-11-18 08:27:45 -0700

    Improved support for UI UseCase by changing showMessageDialog to be used anywhere (not just in ViewModels)

--- library/src/main/java/org/dbtools/android/commons/ui/compose/dialog/DialogUiState.kt
+++ library/src/main/java/org/dbtools/android/commons/ui/compose/dialog/DialogUiState.kt
@@ -3,10 +3,8 @@
 package org.dbtools.android.commons.ui.compose.dialog
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.stringResource
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -46,7 +44,7 @@ fun LibraryDialogs(dialogUiState: DialogUiState<*>) {
     }
 }
 
-fun ViewModel.showMessageDialog(
+fun showMessageDialog(
     dialogUiStateFlow: MutableStateFlow<DialogUiState<*>?>,
     title: @Composable (() -> String)? = null,
     text: @Composable (() -> String)? = null,
@@ -83,7 +81,7 @@ fun ViewModel.showMessageDialog(
     )
 }
 
-fun ViewModel.dismissDialog(
+fun dismissDialog(
     dialogUiStateFlow: MutableStateFlow<DialogUiState<*>?>
 ) {
     dialogUiStateFlow.value = null
