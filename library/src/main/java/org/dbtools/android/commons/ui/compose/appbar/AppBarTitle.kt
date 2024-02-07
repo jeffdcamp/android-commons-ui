06e2f320166a57afe6caccaba9cdfeace48fed52
jeffdcamp <jeffdcamp@gmail.com>
2024-02-07 13:08:34 -0700

    Added Modifier.semantics { heading() } to AppBarTitle

--- library/src/main/java/org/dbtools/android/commons/ui/compose/appbar/AppBarTitle.kt
+++ library/src/main/java/org/dbtools/android/commons/ui/compose/appbar/AppBarTitle.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextOverflow
 import org.dbtools.android.commons.ui.compose.AutoSizeText
 
@@ -23,13 +26,15 @@ fun AppBarTitle(
                 text = title,
                 style = MaterialTheme.typography.titleLarge,
                 maxLines = 1,
-                overflow = titleOverflow
+                overflow = titleOverflow,
+                modifier = Modifier.semantics { heading() },
             )
         } else {
             AutoSizeText(
                 text = title,
                 style = MaterialTheme.typography.titleLarge,
                 maxLines = 1,
+                modifier = Modifier.semantics { heading() },
                 // overflow = TextOverflow.Clip  // NOTE: AutoSizeText can ONLY use TextOverflow.Clip
             )
         }
