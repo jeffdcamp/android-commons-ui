1e277452370ff03c8dc055de3e157695ab835be6
jeffdcamp <jeffdcamp@gmail.com>
2025-02-22 20:16:13 -0700

    Added backgroundColor to ListItemTextHeader

--- library/src/main/java/org/dbtools/android/commons/ui/compose/list/ListItemTextHeader.kt
+++ library/src/main/java/org/dbtools/android/commons/ui/compose/list/ListItemTextHeader.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,16 +17,21 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import org.dbtools.android.commons.ui.compose.ext.transparentContainerColors
 
 @Composable
 fun ListItemTextHeader(
     text: String,
     modifier: Modifier = Modifier,
     color: Color = MaterialTheme.colorScheme.primary,
+    backgroundColor: Color = MaterialTheme.colorScheme.surface,
     style: TextStyle = MaterialTheme.typography.titleSmall,
     textPadding: PaddingValues = PaddingValues(start = 16.dp, top = 16.dp),
 ) {
-    Surface(modifier) {
+    Surface(
+        color = backgroundColor,
+        modifier = modifier
+    ) {
         Column {
             Text(
                 text = text,
@@ -42,22 +49,33 @@ fun ListItemTextHeader(
 @Composable
 private fun Preview() {
     MaterialTheme {
-        Surface {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(10.dp),
-                modifier = Modifier.padding(start = 16.dp)
-            ) {
-                ListItemTextHeader(text = "Food")
-                ListItem(headlineContent = { Text("Pizza") })
-                ListItem(headlineContent = { Text("Taco") })
-                ListItem(headlineContent = { Text("Hot Dog") })
-                ListItem(headlineContent = { Text("Banana") })
+        Surface(
+//            color = Color.LightGray,
+        ) {
+            ElevatedCard {
 
-                ListItemTextHeader(text = "Colors")
-                ListItem(headlineContent = { Text("Red") })
-                ListItem(headlineContent = { Text("Green") })
-                ListItem(headlineContent = { Text("Blue") })
-                ListItem(headlineContent = { Text("Yellow") })
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(10.dp),
+                    modifier = Modifier.padding(start = 16.dp)
+                ) {
+                    ListItemTextHeader(
+                        text = "Food",
+                        backgroundColor = Color.Transparent
+                    )
+                    ListItem(
+                        headlineContent = { Text("Pizza") },
+                        colors = ListItemDefaults.transparentContainerColors(),
+                    )
+                    ListItem(headlineContent = { Text("Taco") })
+                    ListItem(headlineContent = { Text("Hot Dog") })
+                    ListItem(headlineContent = { Text("Banana") })
+
+                    ListItemTextHeader(text = "Colors")
+                    ListItem(headlineContent = { Text("Red") })
+                    ListItem(headlineContent = { Text("Green") })
+                    ListItem(headlineContent = { Text("Blue") })
+                    ListItem(headlineContent = { Text("Yellow") })
+                }
             }
         }
     }
